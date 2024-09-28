194ee8c8e38176c595c70d50b2c92328db46613a
marinadotnet <msichova@outlook.com>
2024-09-28 14:18:52 +0800

    Add new project AuthAccount.API with Order and ApiUser classes.

---
+++ AuthAccount.API/Models/ApiUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthAccount.API.Models;
+
+/// <summary>
+/// Represents an API user that inherits from the IdentityUser class.
+/// Contains a collection of associated orders.
+/// </summary>
+public class ApiUser : IdentityUser
+{
+    /// <summary>
+    /// Navigation property for the collection of orders associated with the user.
+    /// This is a one-to-many relationship where a user can have multiple orders.
+    /// </summary>
+    public virtual ICollection<Order>? Orders { get; set; } = [];
+}
---
+++ AuthAccount.API/Models/Order.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthAccount.API.Models;
+
+/// <summary>
+/// Represents an order made by a user.
+/// </summary>
+[Table("Orders")]
+[PrimaryKey("OrderId")]
+public class Order
+{
+    /// <summary>
+    /// Unique identifier for the order, generated as a GUID.
+    /// </summary>
+    public string OrderId { get; set; } = Guid.NewGuid().ToString();
+
+    /// <summary>
+    /// Foreign key that references the primary key (Id) of the ApiUser.
+    /// Links the order to the user who created it.
+    /// </summary>
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = string.Empty!;
+
+    /// <summary>
+    /// Navigation property to access the user who placed the order.
+    /// Ensures the user who created the order is linked.
+    /// </summary>
+    public ApiUser User { get; set; } = null!;
+
+    /// <summary>
+    /// A list of products added to the order.
+    /// </summary>
+    public List<string>? ProductsAddedToOrder { get; set; } = [];
+
+    /// <summary>
+    /// The total price of the order, with a precision of 18 digits and 2 decimal places.
+    /// </summary>
+    [Precision(18, 2)]
+    public decimal? TotalPrice { get; set; }
+
+    /// <summary>
+    /// Indicates whether the order has been submitted.
+    /// </summary>
+    public bool IsOrderSubmitted { get; set; }
+}
