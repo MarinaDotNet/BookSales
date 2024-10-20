af493922f5f6e70ea53c6a15bffc92a13b6af6fb
marinadotnet <msichova@outlook.com>
2024-10-20 20:07:02 +0800

    Update site.js, added function setActiveNavButton() that adds 'active' class to the class list the button linked to the current page.

--- BookShop.WebApp/wwwroot/js/site.js
+++ BookShop.WebApp/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Adds the 'active' class to the class list the navigation button linked to the current page.
+function setActiveNavButton()
+{
+    const currentPageId = document.body.id;
+
+    if (!currentPageId) {
+        return;
+    }
+
+    const navButtons = document.querySelectorAll(".nav-link");
+
+    for (const button of navButtons) {
+        if (`${button.id}page` === currentPageId) {
+            button.classList.add("active");
+            return;
+        }
+    }
+}
