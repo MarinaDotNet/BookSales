6a8e7e99e52de7d9637287457003940c7c501988
marinadotnet <msichova@outlook.com>
2024-10-20 19:54:16 +0800

    Add ViewModel and ProductViewModel classes.

---
+++ BookShop.WebApp/Models/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ApiUtilities.Models;
+
+namespace BookShop.WebApp.Models;
+
+/// <summary>
+/// ViewModel representing a product with details of a single book and a collection of books.
+/// </summary>
+public class ProductViewModel
+{
+    /// <summary>
+    /// Gets or sets the current book being viewed or managed.
+    /// This is initialized with a new instance of the <see cref="ApiUtilities.Models.Book"/> class by default.
+    /// </summary>
+    public Book Book { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the collection of books.
+    /// By default, this is initialized as an empty collection of <see cref="ApiUtilities.Models.Book"/>.
+    /// </summary>
+    public IEnumerable<Book> Books { get; set; } = [];
+}
---
+++ BookShop.WebApp/Models/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace BookShop.WebApp.Models;
+
+/// <summary>
+/// ViewModel representing various data related to product, filtering, ordering, and errors.
+/// </summary>
+public class ViewModel
+{
+    /// <summary>
+    /// Gets or sets the genres associated with the current product.
+    /// Initialized with a default value of <c>"undefined"</c>.
+    /// </summary>
+    public string[] Genres { get; set; } = ["undefined"];
+
+    /// <summary>
+    /// Gets or sets the product-related information.
+    /// Initialized with a new instance of the <see cref="ProductViewModel"/> class.
+    /// </summary>
+    public ProductViewModel Product { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the filtering criteria for products.
+    /// Initialized with a new instance of the <see cref="FilterModel"/> class.
+    /// </summary>
+    public FilterModel Filter { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the ordering information for sorting products.
+    /// Initialized with a new instance of the <see cref="OrderViewModel"/> class.
+    /// </summary>
+    public OrderViewModel Order { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the error details if an error occurs.
+    /// Initialized with a new instance of the <see cref="ErrorViewModel"/> class.
+    /// </summary>
+    public ErrorViewModel ErrorView { get; set; } = new();
+}
