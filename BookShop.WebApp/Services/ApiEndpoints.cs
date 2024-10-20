cac0942abbed41923386c1d2107087f69ae914d9
marinadotnet <msichova@outlook.com>
2024-10-20 19:38:20 +0800

    Add class ApiEndPoints.cs to hold API endpoints for the web application.

---
+++ BookShop.WebApp/Services/ApiEndpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace BookShop.WebApp.Services;
+
+/// <summary>
+/// Static class that holds API endpoints for the application.
+/// </summary>
+public static class ApiEndpoints
+{
+    /// <summary>
+    /// Base URL for the Book API. This should be set to the root URL of the Book API service.
+    /// </summary>
+    public static string BaseBookApiUrl {  get; set; } = string.Empty!;
+
+    /// <summary>
+    /// Static class that contains endpoints related to books.
+    /// </summary>
+    public static class Books
+    {
+        /// <summary>
+        /// Gets the endpoint for retrieving all books.
+        /// </summary>
+        /// <value>A string representing the URL to fetch all books.</value>
+        public static string GetAll => $"{BaseBookApiUrl}/all";
+    }
+}
