87b3eee5e24fb6210bafff85a394bb62eded312c
marinadotnet <msichova@outlook.com>
2024-10-21 17:43:39 +0800

    Add MemoryCache, AppDbContext and Identity Services to DI.

--- BookShop.WebApp/Program.cs
+++ BookShop.WebApp/Program.cs
@@ -1,9 +1,18 @@
+using ApiUtilities.Models;
+using ApiUtilities.Services;
 using BookShop.WebApp.Services;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddUserSecrets<StartupBase>();
 ApiEndpoints.BaseBookApiUrl = builder.Configuration["ApiEndpointsSettings:BaseBookApiUrl"]!;
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddIdentity<ApiUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
