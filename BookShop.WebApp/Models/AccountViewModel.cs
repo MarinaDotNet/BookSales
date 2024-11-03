1e1ccb3ce022fbcdc922e00dcf65e3e10ba4db34
marinadotnet <msichova@outlook.com>
2024-11-03 17:37:37 +0800

    Add AccountViewModel class to represent user account information

---
+++ BookShop.WebApp/Models/AccountViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace BookShop.WebApp.Models;
+
+/// <summary>
+/// Represents the view model for an account, holding user information 
+/// and registration-related details.
+/// </summary>
+public class AccountViewModel
+{
+    /// <summary>
+    /// Gets or sets the name of the user.
+    /// </summary>
+    /// <value>
+    /// The name of the user.
+    /// </value>
+    public string UserName { get; set; } = string.Empty!;
+
+    /// <summary>
+    /// Gets or sets the email.
+    /// </summary>
+    /// <value>
+    /// The email.
+    /// </value>
+    public string Email { get; set; } = string.Empty!;
+
+    /// <summary>
+    /// Gets or sets the password.
+    /// </summary>
+    /// <value>
+    /// The password.
+    /// </value>
+    public string Password { get; set; } = string.Empty!;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this instance is email confirmed.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if this instance is email confirmed; otherwise, <c>false</c>.
+    /// </value>
+    public bool IsEmailConfirmed { get; set; }
+
+    /// <summary>
+    /// Gets or sets the registration message.
+    /// </summary>
+    /// <value>
+    /// The registration message.
+    /// </value>
+    public string? RegistrationMessage { get; set; } 
+}
