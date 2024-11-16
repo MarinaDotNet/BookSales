d7960dbd3d32cf2f3717238e34a9f00f1eb34658
marinadotnet <msichova@outlook.com>
2024-11-16 10:49:45 +0800

    Add FilterModel class that represents filter details for a Books collection.

---
+++ BookShop.WebApp/Models/FilterModel.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop.WebApp.Models;
+
+/// <summary>
+/// Represents filter details for a collection of items.
+/// </summary>
+public class FilterModel
+{
+    /// <summary>
+    /// Gets or sets the selected genre.
+    /// </summary>
+    /// <value>
+    /// The selected genre.
+    /// </value>
+    public string SelectedGenre { get; set; } = "undefined";
+
+    /// <summary>
+    /// Gets or sets the search tearm.
+    /// </summary>
+    /// <value>
+    /// The search tearm.
+    /// </value>
+    public string SearchTearm { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this instance is ascending order.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if this instance is ascending order; otherwise, <c>false</c>.
+    /// </value>
+    public bool IsAscendingOrder { get; set; } = true;
+}
